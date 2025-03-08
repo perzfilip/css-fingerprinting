1fea18fb2e54aa19990bc2da06f97dd1445c2239
robsonwp <perzfilip@gmail.com>
2025-03-08 16:22:23 +0100

    sample config

---
+++ sample_config.py
@@ -0,0 +1,14 @@
+# config file
+from datetime import timedelta
+import logging
+
+# static secret key for consistency
+SECRET_KEY = 'FILL_ME_IN'
+
+# not expiring sessions
+PERMANENT_SESSION_LIFETIME = timedelta(days=365*100)
+
+# SQLite database
+SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+LOGGING_LEVEL = logging.DEBUG
